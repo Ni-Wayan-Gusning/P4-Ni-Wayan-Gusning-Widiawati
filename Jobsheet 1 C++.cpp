bb2eed871cbb5f6dbec3d90f37fdbadd2db1b1d1
NI WAYAN GUSNING WIDIAWATI <64563488+Ni-Wayan-Gusning@users.noreply.github.com>
2020-05-11 22:45:37 -0700

    Create Jobsheet 1 C++.cpp
    Program array satu dimensi yang menampilkan NIM masing – masing mahasiswa

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Ni Wayan Gusning Widiawati-F1B019106-kelompok 16\n\n";
+int nim[6]= {6,12,7,9,8,9};
+	
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "Nim ke-2 adalah " << nim[1] << endl;
+	cout << "Nim ke-3 adalah " << nim[2] << endl;
+	cout << "Nim ke-4 adalah " << nim[3] << endl;
+	cout << "Nim ke-5 adalah " << nim[4] << endl;
+	cout << "Nim ke-6 adalah " << nim[4] << endl;
+
+	
+
+}
+
