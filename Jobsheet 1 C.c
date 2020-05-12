8c4bd880de56f141e9900e2453fc7a55aa1a40d7
NI WAYAN GUSNING WIDIAWATI <64563488+Ni-Wayan-Gusning@users.noreply.github.com>
2020-05-11 22:41:57 -0700

    Create Jobsheet 1 C.c
    Program array satu dimensi yang menampilkan NIM masing – masing mahasiswa

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+main (){
+	printf("NI WAYAN GUSNING WIDIAWATI-F1B019106-KELOMPOK 16 \n");
+
+	int nim[6]= {14,3,15,49,37,44};
+	
+	printf ("Nim ke-1 = %i\n",nim[0]);
+	printf ("Nim ke-2 = %i\n",nim[1]);
+	printf ("Nim ke-3 = %i\n",nim[2]);
+	printf ("Nim ke-4 = %i\n",nim[3]);
+	printf ("Nim ke-5 = %i\n",nim[4]);
+	printf ("Nim ke-6 = %i\n",nim[5]);
+	printf ("Nim ke-7 = %i\n",nim[6]);
+	
+}
