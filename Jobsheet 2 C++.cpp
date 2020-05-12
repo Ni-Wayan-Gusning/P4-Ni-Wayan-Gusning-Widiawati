7d4cd361a1c6dcd42329a73d17b72d6d36441142
NI WAYAN GUSNING WIDIAWATI <64563488+Ni-Wayan-Gusning@users.noreply.github.com>
2020-05-11 22:52:06 -0700

    Create Jobsheet 2 C++.cpp
    program mengisi dan menampilkan elemen array dengan index array sesuai dengan NIM terakhir setiap Mahasiswa

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	cout<<"Nama     : NI WAYAN GUSNING WIDIAWATI"<<endl;
+	cout<<"Nim      : F1B019106"<<endl;
+	cout<<"Kelompok : 16"<<endl;
+	
+	int a [6];
+	
+	cout<<"Input Nilai ke-1 : ";
+	cin>>a[0];
+	
+	cout<<"Input Nilai ke-2 : ";
+	cin>>a[1];
+	
+	cout<<"Input Nilai ke-3 : ";
+	cin>>a[2];
+	
+	cout<<"Input Nilai ke-4 : ";
+	cin>>a[3];
+	
+	cout<<"Input Nilai ke-5 : ";
+	cin>>a[4];
+	
+	cout<<"Input Nilai ke-6 : ";
+	cin>>a[5];		
+}
